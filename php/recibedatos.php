c637ab3e87bc1193b20cd2c0b59c206f0ec7cb18
asteriscoGithub1KikoSanJuan <kikosanjuan@gmail.com>
2017-02-21 15:39:03 +0100

    contacto img php

---
+++ php/recibedatos.php
@@ -0,0 +1,19 @@
+<?php
+echo "<h1>Hola, aquí llegará el form</h1>";
+if($_POST) { 
+	echo "Llegan variables por post:<br>";
+	echo $_POST['nombreApellido'];
+	echo "<br>";
+	echo $_POST['email'];
+	echo "<br>";
+	echo $_POST['mensaje'];
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'takeaway');
+	if ($mysqli){
+		$sql="";
+	}
+	$mysqli->close();
+	}
+	else { 
+	echo "No llega nada por post";
+	}
+?>
