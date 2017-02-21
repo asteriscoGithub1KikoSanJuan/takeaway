267524f8e0210d4e41004452a1bb6e28e8222d98
asteriscoGithub1KikoSanJuan <kikosanjuan@gmail.com>
2017-02-21 16:42:25 +0100

    commit  index php

--- php/recibedatos.php
+++ php/recibedatos.php
@@ -1,5 +1,6 @@
 <?php
 echo "<h1>Hola, aquí llegará el form</h1>";
+//Se comprueba que llegan datos por methodo POST
 if($_POST) { 
 	echo "Llegan variables por post:<br>";
 	echo $_POST['nombreApellido'];
@@ -9,7 +10,14 @@ if($_POST) {
 	echo $_POST['mensaje'];
 	$mysqli = new mysqli('127.0.0.1', 'root', '', 'takeaway');
 	if ($mysqli){
-		$sql="";
+		$sql="INSERT INTO takeaway.contactform ( nombreApellido, email, mensaje) VALUES ( 'asensio', 'asensio@gdsk', 'me comprao 20 pavos');";
+		$query=mysqli->query($sql);
+		if ($$query) {
+			echo "se ha grabado correctamente ls informscion"
+		}
+		{
+			echo "ha habido un problema con el registro del formulario";
+		}
 	}
 	$mysqli->close();
 	}
